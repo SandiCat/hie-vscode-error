4a396208cdbfb247b029716745a945e7e87d1195
Sandi <dsandi333@gmail.com>
2019-08-04 05:30:55 +0200

    initial

---
+++ src/Main.hs
@@ -0,0 +1,18 @@
+
+{-# LANGUAGE  OverloadedStrings #-}
+{-# LANGUAGE  DeriveGeneric #-}
+{-# LANGUAGE  DeriveAnyClass #-}
+{-# LANGUAGE  NamedFieldPuns #-}
+{-# LANGUAGE  TypeSynonymInstances #-}
+{-# LANGUAGE  FlexibleInstances #-}
+{-# LANGUAGE  QuasiQuotes #-}
+{-# LANGUAGE  StandaloneDeriving #-}
+{-# LANGUAGE  StrictData #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+
+module Main where
+
+
+main :: IO ()
+main = return ()
